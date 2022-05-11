b1679c7fd648e1b9f7f31aad206394102cf9bc85
David Uzziel Mena Maldonado <dmenam@nhelios.com.mx>
2022-05-11 10:22:46 -0500

    Paso de datos a la vista

--- src/main/java/com/springboot/web/controllers/IndexController.java
+++ src/main/java/com/springboot/web/controllers/IndexController.java
@@ -1,13 +1,15 @@
 package com.springboot.web.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
 	
 	@GetMapping({"/index","/",""})
-	public String index() {
+	public String index(Model model) {
+		model.addAttribute("titulo", "Hola spring Model!");
 		return "index";
 	}
 
