5e4372d2012a085604ad242cd07d5801c6c43e17
David Uzziel Mena Maldonado <dmenam@nhelios.com.mx>
2022-05-13 15:14:31 -0500

    Directiva if y foreach Thymeleaf

--- src/main/java/com/springboot/web/controllers/IndexController.java
+++ src/main/java/com/springboot/web/controllers/IndexController.java
@@ -1,5 +1,9 @@
 package com.springboot.web.controllers;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.springboot.web.models.Usuario;
 
+@RequestMapping("/app")
 @Controller
 public class IndexController {
 	
@@ -22,9 +27,27 @@ public class IndexController {
 		Usuario usuario = new Usuario();
 		usuario.setNombre("David");
 		usuario.setApellido("Mena");
+		usuario.setEmail("dumm2000@gmail.com");
 		model.addAttribute("usuario", usuario);
 		model.addAttribute("titulo", "Perfil del usuario: ".concat(usuario.getNombre()));
 		return "perfil";
 	}
-
+	
+	@RequestMapping("/listar")
+	public String listar(Model model) {
+		List<Usuario> usuarios = Arrays.asList(new Usuario("David","Mena","dumm2000@gmail.com"),
+				new Usuario("Irma","Maldonado","irma@gmail.com"),
+				new Usuario("Juan","Mena","juan@gmail.com"));
+		
+		/* Metodo 2 menos limpio
+		List<Usuario> usuarios = new ArrayList<Usuario>();
+		usuarios.add(new Usuario("David","Mena","dumm2000@gmail.com"));
+		usuarios.add(new Usuario("Irma","Maldonado","irma@gmail.com"));
+		usuarios.add(new Usuario("Juan","Mena","juan@gmail.com"));
+		*/
+		
+		model.addAttribute("titulo", "Lista de usuarios");
+		model.addAttribute("usuarios", usuarios);
+		return "listar";
+	}
 }
